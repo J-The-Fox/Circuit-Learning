dbb5057c58c52e9ba62e51cd11d2a0809db54d8b
J-The-Fox <116409406+J-The-Fox@users.noreply.github.com>
2023-04-04 17:34:04 -0500

    Started Input.py

---
+++ modules/input.py
@@ -0,0 +1,52 @@
+"""
+This File Holds Various Ways To Get Input From The User Using Text
+"""
+
+# TODO: Fix The Input Box. (Pulled From https://stackoverflow.com/questions/46390231/how-can-i-create-a-text-input-box-with-pygame)
+# TODO: Add Other Input Methods (Combine The Modules 'button' And 'menu' Into This File)
+
+import pygame
+
+pygame.init()
+
+class InputBox: 
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
