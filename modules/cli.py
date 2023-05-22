2196ffa04311685e806fa141f98c6665684816b4
J-The-Fox <j.t.mcneely06@gmail.com>
2023-05-22 07:59:26 -0500

    Split _IMPORTERROR

--- modules/cli.py
+++ modules/cli.py
@@ -1,24 +1,30 @@
 import os
 import sys
+import json
 import configparser
 # External Modules
-_IMPORTERROR = []
+_MISSINGREQUIRED = []
+_MISSINGOPTIONAL = []
 try:
     import pygame
 except ImportError:
-    _IMPORTERROR.append("pygame")
+    _MISSINGREQUIRED.append("pygame")
 try:
     import requests
 except ImportError:
-    _IMPORTERROR.append("requests")
+    _MISSINGOPTIONAL.append("requests")
 try:
     import psutil
 except ImportError:
-    _IMPORTERROR.append("psutil")
+    _MISSINGOPTIONAL.append("psutil")
 try:
     import pygame_gui
 except ImportError:
-    _IMPORTERROR.append("pygame_gui")
+    _MISSINGREQUIRED.append("pygame_gui")
+try:
+    import hashlib
+except ImportError:
+    _MISSINGOPTIONAL.append("hashlib")
 # Custom Modules
 import logger
 import decorators
@@ -47,15 +53,47 @@ _logging = logger.Logger(
 
 del _log_path, _logging_config
 
-# Check For Missing Modules
-if len(_IMPORTERROR) > 0:
-    _logging.write("The Following Modules Could Not Be Imported: {}".format(", ".join(_IMPORTERROR)), logger.Logger.CRITICAL)
-    _logging.write("Please Install The Missing Modules And Try Again", logger.Logger.CRITICAL)
-    generate_debug_log(extra_info="    The Following Modules Could Not Be Imported: {}. \n    Exit Code 1.".format(", ".join(_IMPORTERROR)))
+# Check For Missing Modules. If Any Required Modules Are Missing, Exit The Program
+# Required Modules: pygame - Used For The GUI 
+#                   pygame_gui - Extends Pygame GUI
+# Optional Modules: requests - Used For Checking For Updates 
+#                   hashlib - Used For Generating Hashes, 
+#                   psutil - Used For Getting System Information And Monitoring
+if len(_MISSINGREQUIRED) > 0:
+    _logging.write("Missing Required Modules: {}".format(", ".join(_MISSINGREQUIRED)), lvl=logger.Logger.CRITICAL)
+    _logging.write("Please Install Them And Try Again", lvl=logger.Logger.ERROR)
+    generate_debug_log(extra_info="Missing Required Modules: {}".format(", ".join(_MISSINGREQUIRED)))
     _logging.close(1)
     sys.exit(1)
+_logging.write("All Required Modules Are Installed!", lvl=logger.Logger.INFO)
+if len(_MISSINGOPTIONAL) > 0:
+    _logging.write("Missing Optional Modules: {}".format(", ".join(_MISSINGOPTIONAL)), lvl=logger.Logger.WARNING)
+    _logging.write("Some Features May Not Work Correctly Or Not At All", lvl=logger.Logger.NOTICE)
 
-_logging.write("All Modules Are Installed!", lvl=logger.Logger.INFO)
+del _MISSINGREQUIRED, _MISSINGOPTIONAL # Clean Up Memory
+
+
+# Generate A Hash File If One Does Not Exist
+try:
+    if os.path.exists(os.path.join(os.path.dirname(__file__).replace("modules", "docs"), ".cache", "hashes.json")) is False:
+        _logging.write("Generating Hash File...", lvl=logger.Logger.INFO)
+        new_files = []
+        new_hashes = {}
+        for root, dirs, files in os.walk(os.path.dirname(__file__)):
+            for file in files:
+                if str(file).endswith(".pyc") is False:
+                    new_files.append(file)
+                    new_hashes[file] = hashlib.md5(open(os.path.join(root, file), 'rb').read()).hexdigest()
+    
+        # Write The Hashes To The Hash File
+        with open(os.path.join(os.path.dirname(__file__).replace("modules", "docs"), ".cache", "hashes.json"), "w") as hash_file:
+            json.dump(new_hashes, hash_file, indent=4)
+            hash_file.close()
+        _logging.write("Stored In {}".format(os.path.join(os.path.dirname(__file__).replace("modules", "docs"), ".cache", "hashes.json")), lvl=logger.Logger.INFO)
+        del hash_file
+except NameError as e:
+    _logging.write("An Error Occurred While Generating The Hash File.", logger.Logger.ERROR, extra_msg=e)
+
 del _logging
 
 import Circuit_Learning
